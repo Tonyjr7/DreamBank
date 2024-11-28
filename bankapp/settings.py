669c6d7a930058993d7b108b2e6e89d14b12dccb
Tonyjr7 <anthonyayokunle777@gmail.com>
2024-11-28 23:22:52 +0100

    'update'

--- bankapp/settings.py
+++ bankapp/settings.py
@@ -32,7 +32,7 @@ environ.Env.read_env()
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'dreambank.onrender.com']
 
