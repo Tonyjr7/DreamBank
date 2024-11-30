141b3670091155a354b369ab9fe73866268160f3
Tonyjr7 <anthonyayokunle777@gmail.com>
2024-11-30 12:36:06 +0100

    'update'

--- bankapp/settings.py
+++ bankapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*aw6t6nv@qh^116=^jqp@f)=be%*nn^gxvgahp0e&p4h98(%d('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'dreambank.onrender.com']
 
